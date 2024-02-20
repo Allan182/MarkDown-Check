9bafcfa7905195eca0bf62e250edf88e457a4146
Allan182 <allan.furlani@hotmail.com>
2024-02-20 18:44:22 -0300

    Captura de Diretorios atráves de prompts

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,26 @@
 import pegaArquivo from "./index.js";
+import fs from 'fs';
+
 
 const path = process.argv;
- 
-async function processText(path){
-    const result = await pegaArquivo(path[2]);
-    console.log(result);
+
+
+function printList(result) {
+    console.log("Lista de Links", result);
+}
+
+async function processText(args) {
+    const path = args[2];
+    if (fs.lstatSync(path).isFile()) {
+        const result = await pegaArquivo(path);
+        printList(result)
+    } else if (fs.lstatSync(path).isDirectory()) {
+        const files = await fs.promises.readdir(path);
+        files.forEach(async (nameFile) => {
+            const list = await pegaArquivo(`${path}/${nameFile}`)
+            printList(list);
+        });
+    }
 }
 
 processText(path);
